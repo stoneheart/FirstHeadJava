7592ba09f4e285e4739faab31a9f6a7b0ae7ab88
stoneheart <kz9892@gmail.com>
2018-08-20 12:04:11 -0700

    Complete Chapter 10

---
+++ Ch10/NoStatic.java
@@ -0,0 +1,7 @@
+import java.lang.Math;
+class NoStatic {
+    public static void main(String[] args) {
+        System.out.println("sqrt " + Math.sqrt(2.0));
+        System.out.println("tan " + Math.tan(60));
+    }
+}
---
+++ Ch10/WithStatic.java
@@ -0,0 +1,8 @@
+import static java.lang.System.out;
+import static java.lang.Math.*;
+class WithStatic {
+    public static void main(String[] args) {
+        out.println("sqrt " + sqrt(2.0));
+        out.println("tan " + tan(60));
+    }
+}
