f6d05cdf9d1f728cdbcdbe10d1aed2a8b91ac1dc
stoneheart <kz9892@gmail.com>
2018-08-21 16:01:43 -0700

    Complete Chapter 11

---
+++ Ch11/ExTestDrive.java
@@ -0,0 +1,25 @@
+class MyEx extends Exception {}
+
+public class ExTestDrive {
+    static void doRisky(String t) throws MyEx {
+        System.out.print("h");
+        if ("yes".equals(t)) {
+            throw new MyEx();
+        }
+    }
+
+    public static void main(String[] args) {
+        String test = args[0];
+        System.out.print("t");
+        try {
+            doRisky(test);
+            System.out.print("r");
+            System.out.print("o");
+        } catch (MyEx e) {
+            System.out.print("a");
+        } finally {
+            System.out.print("w");
+            System.out.println("s");
+        }
+    }
+}
